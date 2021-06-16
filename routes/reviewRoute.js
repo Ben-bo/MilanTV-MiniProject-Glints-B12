79f8a9e812a0626ca2b3ccb60fe50a31b2eed83e
ILHAM ADJI BINTANG <bintangb21@gmail.com>
2021-06-16 22:43:00 +0800

    Create Middleware to Validate Request Body

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const reviewMiddleware = require("../middlewares/reviewMiddleware");
+const validateRequest = require("../middlewares/validateRequestMiddleware");
 const reviewController = require("../controllers/reviewController");
 const authorizationMiddleware = require("../middlewares/authorizationMiddleware");
 
 router.post(
   "/:movie_id",
   authorizationMiddleware.userAuthorization,
-  reviewMiddleware.validation,
+  validateRequest.addReview,
   reviewController.create
 );
 
