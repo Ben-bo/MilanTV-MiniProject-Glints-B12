8b20ec0d74d4353c55bff1e51806384d015fe992
lamseboen <stephenlambok@gmail.com>
2021-06-17 18:35:11 +0700

    q

--- database/config/config.js
+++ database/config/config.js
@@ -10,11 +10,11 @@ const {
 
 module.exports = {
   "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "port": DB_PORT,
+    "username": "oajntzkuxbfkwi",
+    "password": "89c6b566c31b15cbabd28e438078127a74906727f127b1229b1a19bd34d4db07",
+    "database": "d6gjm40pitabhc",
+    "host": "ec2-3-233-7-12.compute-1.amazonaws.com",
+    "port": "5432",
     "dialect": "postgres",
     "ssl": true,
     "dialectOptions": {
@@ -46,8 +46,10 @@ module.exports = {
     "dialect": "postgres",
     "ssl": true,
     "dialectOptions": {
-      "ssl": true,
-      "rejectUnauthorized": false
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
     }
   }
 }
